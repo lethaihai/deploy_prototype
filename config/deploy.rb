eee70e9bf909f3dde4a147d504618c1a94a547ef
hailt <hailt@rikkeisoft.com>
2020-01-06 15:51:00 +0700

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 lock "~> 3.11.0"
 
 set :application, "prototype"
-set :repo_url, "git@github.com:duongpham910/deploy_prototype.git"
+set :repo_url, "git@github.com:lethaihai/deploy_prototype.git"
 set :bundle_binstubs, nil
 
 # Default branch is :master
@@ -20,6 +20,19 @@ after "deploy:publishing", "deploy:restart"
 
 # Khởi động lại unicorn sau khi deploy
 namespace :deploy do
+
+  desc "create database"
+  task :create_database do
+    on roles(:db) do |host|
+      within "#{release_path}" do
+        with rails_env: ENV["staging"] do
+          execute :rake, "RAILS_ENV=staging db:create"
+        end
+      end
+    end
+  end
+  before :migrate, :create_database
+
   task :restart do
     invoke "unicorn:restart"
   end
